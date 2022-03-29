5f04a9b75b2c7f2e39a246d91fc4ded6bb5a6118
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-03-29 12:01:33 -0500

    Update app.js

--- Financial Graph/app.js
+++ Financial Graph/app.js
@@ -4,11 +4,15 @@ const ctx = chart.getContext("2d");
 const candleWidth = 20;
 const numCandles = 50;
 
+const yTop = 100; // top offset
+const xLeft = 100; // left offset
+
+const yAxisXLeft = 75;
+const yAxisLabelXLeft = 25;
+
+
 var height = document.getElementById('myChart').clientHeight;
 var width = document.getElementById('myChart').clientWidth;
-
-let yTop = 400; // top offset
-let xLeft = 100; // left offset
 
 let hMax = 0;
 let lMin = 0;
@@ -17,52 +21,66 @@ let xInc = (width - xLeft) / numCandles; // distance between each candle
 
 var o, c, h, l;
 
+var candles = new Array;
+
 
 class CandleStick{
-    constructor(i){
-        this.draw(i);
+    constructor(o, c, h, l, t){
+        this.o = o;
+        this.c = c;
+        this.h = h;
+        this.l = l;
+        this.t = t;
+        this.draw(o, c, h, l, t);
     }
 
-    draw(i){
+    draw(o, c, h, l, t){
         if(o < c){
             ctx.fillStyle = "#FF0000";
-        }else{
-            ctx.fillStyle = "#00FF00";
-        }
-        ctx.beginPath();
-        ctx.fillRect(xLeft + xInc * i, yTop + o, candleWidth, c - o);
-        ctx.stroke();
-    
-        if(o < c){
             ctx.strokeStyle = "#FF0000";
         }else{
+            ctx.fillStyle = "#00FF00";
             ctx.strokeStyle = "#00FF00";
         }
         ctx.beginPath();
-        ctx.moveTo(xLeft + xInc * i + candleWidth/2, yTop + h);
-        ctx.lineTo(xLeft + xInc * i + candleWidth/2, yTop + l);
+        ctx.fillRect(xLeft + xInc * t, o + yTop, candleWidth, c - o);
+        ctx.stroke();
+    
+        ctx.beginPath();
+        ctx.moveTo(xLeft + xInc * t + candleWidth/2, h + yTop);
+        ctx.lineTo(xLeft + xInc * t + candleWidth/2, l + yTop);
         ctx.stroke();
     }
 }
 
 
-for(let i = 0; i < numCandles; i++){
+for(let t = 0; t < numCandles; t++){
     // randomize values
-    o = Math.random() * 100;
-    c = Math.random() * 100;
-    h = o - Math.random() * 50;
-    l = c + Math.random() * 50;
+    o = Math.random() * (height - yTop * 2);
+    c = Math.random() * (height - yTop * 2);
+    h = Math.min(o, c) - Math.random() * 100;
+    l = Math.max(o, c) + Math.random() * 100;
 
     // update hMax and hMin
     hMax = Math.max(hMax, h);
     lMin = Math.min(lMin, l);
 
-    let candle = new CandleStick(i);
-    candle.draw(i);
+    let candle = new CandleStick(o, c, h, l, t);
+    candles.push(candle);
 }
 
-hMax += yTop;
 lMin += yTop;
+hMax += yTop;
 
+// draw axis
+ctx.strokeStyle = "#000000";
+ctx.fillStyle = "#000000";
+ctx.beginPath();
+ctx.moveTo(yAxisXLeft, 0);
+ctx.lineTo(yAxisXLeft, height);
+ctx.stroke();
 
-
+for(let i = lMin; i <= hMax; i += (hMax - lMin) / 10){
+    ctx.font = "10px Arial";
+    ctx.fillText(Math.round(i), yAxisLabelXLeft, i);
+}
