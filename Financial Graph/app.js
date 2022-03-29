0812d8bf3e5e6cfc36ea90b1a3cff9e5e09ef37e
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-03-29 10:48:01 -0500

    Update app.js

--- Financial Graph/app.js
+++ Financial Graph/app.js
@@ -1,25 +1,68 @@
-const c = document.getElementById('myChart');
-const ctx = c.getContext("2d");
+const chart = document.getElementById('myChart');
+const ctx = chart.getContext("2d");
 
 const candleWidth = 20;
+const numCandles = 50;
 
 var height = document.getElementById('myChart').clientHeight;
 var width = document.getElementById('myChart').clientWidth;
 
-const myData = JSON.parse('{"o":["40","30","20"], "c":["70","20","10"], "h":["40","80","150"], "l":["50","30","90"], "d":["2020-01-11","2020-01-12","2020-01-13"]}');
+let yTop = 400; // top offset
+let xLeft = 100; // left offset
+
+let hMax = 0;
+let lMin = 0;
+
+let xInc = (width - xLeft) / numCandles; // distance between each candle
+
+var o, c, h, l;
 
 
-let xInc = width/myData.o.length;
+class CandleStick{
+    constructor(i){
+        this.draw(i);
+    }
 
-for(let i = 0; i < myData.o.length; i++){
-    // draw candlestick
-    ctx.beginPath();
-    ctx.fillRect(xInc * i, myData.o[i], candleWidth, myData.c[i]);
-    ctx.stroke();
-
-    // draw line
-    ctx.beginPath();
-    ctx.moveTo(xInc * i + candleWidth/2, myData.l[i]);
-    ctx.lineTo(xInc * i + candleWidth/2, myData.h[i]);
-    ctx.stroke();
+    draw(i){
+        if(o < c){
+            ctx.fillStyle = "#FF0000";
+        }else{
+            ctx.fillStyle = "#00FF00";
+        }
+        ctx.beginPath();
+        ctx.fillRect(xLeft + xInc * i, yTop + o, candleWidth, c - o);
+        ctx.stroke();
+    
+        if(o < c){
+            ctx.strokeStyle = "#FF0000";
+        }else{
+            ctx.strokeStyle = "#00FF00";
+        }
+        ctx.beginPath();
+        ctx.moveTo(xLeft + xInc * i + candleWidth/2, yTop + h);
+        ctx.lineTo(xLeft + xInc * i + candleWidth/2, yTop + l);
+        ctx.stroke();
+    }
 }
+
+
+for(let i = 0; i < numCandles; i++){
+    // randomize values
+    o = Math.random() * 100;
+    c = Math.random() * 100;
+    h = o - Math.random() * 50;
+    l = c + Math.random() * 50;
+
+    // update hMax and hMin
+    hMax = Math.max(hMax, h);
+    lMin = Math.min(lMin, l);
+
+    let candle = new CandleStick(i);
+    candle.draw(i);
+}
+
+hMax += yTop;
+lMin += yTop;
+
+
+
