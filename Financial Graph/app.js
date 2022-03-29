cc290550d8e2191edbe5247cd39dc043f2b2419b
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-03-29 18:24:43 -0500

    Update app.js

--- Financial Graph/app.js
+++ Financial Graph/app.js
@@ -1,6 +1,11 @@
 const chart = document.getElementById('myChart');
 const ctx = chart.getContext("2d");
 
+chart.addEventListener("mousemove", function(event) {
+    crosshair(event);
+});
+document.addEventListener("DOMContentLoaded", startAnimation);
+
 const candleWidth = 20;
 const numCandles = 50;
 
@@ -14,8 +19,8 @@ const yAxisLabelXLeft = 25;
 var height = document.getElementById('myChart').clientHeight;
 var width = document.getElementById('myChart').clientWidth;
 
-let hMax = 0;
-let lMin = 0;
+var hMax = 0;
+var lMin = height;
 
 let xInc = (width - xLeft) / numCandles; // distance between each candle
 
@@ -24,63 +29,106 @@ var o, c, h, l;
 var candles = new Array;
 
 
-class CandleStick{
-    constructor(o, c, h, l, t){
+class CandleStick {
+    constructor(o, c, h, l, t) {
         this.o = o;
         this.c = c;
         this.h = h;
         this.l = l;
         this.t = t;
         this.draw(o, c, h, l, t);
+
+        var div = document.createElement('div');
+        var canvas = document.createElement('canvas');
+        canvas.width = candleWidth;
+
+        div.appendChild(canvas);
+        document.getElementsByTagName('body')[0].appendChild(div);
+        div.addEventListener("mouseover", displayInfo);
     }
 
-    draw(o, c, h, l, t){
-        if(o < c){
+    draw(o, c, h, l, t) {
+        if (o < c) {
             ctx.fillStyle = "#FF0000";
             ctx.strokeStyle = "#FF0000";
-        }else{
+        } else {
             ctx.fillStyle = "#00FF00";
             ctx.strokeStyle = "#00FF00";
         }
         ctx.beginPath();
-        ctx.fillRect(xLeft + xInc * t, o + yTop, candleWidth, c - o);
+        let smallerOC = Math.min(o, c);
+        let largerOC = Math.max(o, c);
+        ctx.fillRect(xLeft + xInc * t, convertY(largerOC), candleWidth, largerOC - smallerOC);
         ctx.stroke();
-    
+
         ctx.beginPath();
-        ctx.moveTo(xLeft + xInc * t + candleWidth/2, h + yTop);
-        ctx.lineTo(xLeft + xInc * t + candleWidth/2, l + yTop);
+        ctx.moveTo(xLeft + xInc * t + candleWidth / 2, convertY(h));
+        ctx.lineTo(xLeft + xInc * t + candleWidth / 2, convertY(l));
         ctx.stroke();
     }
 }
 
+function startAnimation() {
+    setInterval(() => {
+        ctx.clearRect(0, 0, width, height);
 
-for(let t = 0; t < numCandles; t++){
-    // randomize values
-    o = Math.random() * (height - yTop * 2);
-    c = Math.random() * (height - yTop * 2);
-    h = Math.min(o, c) - Math.random() * 100;
-    l = Math.max(o, c) + Math.random() * 100;
-
-    // update hMax and hMin
-    hMax = Math.max(hMax, h);
-    lMin = Math.min(lMin, l);
-
-    let candle = new CandleStick(o, c, h, l, t);
-    candles.push(candle);
+        // draw();
+    }, 1);
 }
 
-lMin += yTop;
-hMax += yTop;
+function displayInfo() {
 
-// draw axis
-ctx.strokeStyle = "#000000";
-ctx.fillStyle = "#000000";
-ctx.beginPath();
-ctx.moveTo(yAxisXLeft, 0);
-ctx.lineTo(yAxisXLeft, height);
-ctx.stroke();
+}
 
-for(let i = lMin; i <= hMax; i += (hMax - lMin) / 10){
-    ctx.font = "10px Arial";
-    ctx.fillText(Math.round(i), yAxisLabelXLeft, i);
+function crosshair(e) {
+    // vertical line
+    ctx.beginPath();
+    ctx.setLineDash([5, 15]);
+    ctx.moveTo(e.clientX, 0);
+    ctx.lineTo(e.clientX, height);
+    ctx.stroke();
+
+    // horizontal line
+    ctx.beginPath();
+    ctx.setLineDash([5, 15]);
+    ctx.moveTo(0, e.clientY);
+    ctx.lineTo(width, e.clientY);
+    ctx.stroke();
+}
+
+function firstDraw() {
+    // draw candles
+    for (let t = 0; t < numCandles; t++) {
+        // randomize values
+        o = Math.random() * (height - 100);
+        c = Math.random() * (height - 100);
+        h = Math.max(o, c) + Math.random() * 50;
+        l = Math.min(o, c) - Math.random() * 50;
+
+        // update hMax and hMin
+        hMax = Math.max(hMax, h);
+        lMin = Math.min(lMin, l);
+
+        let candle = new CandleStick(o, c, h, l, t);
+        candles.push(candle);
+    }
+
+
+    // draw axis
+    ctx.strokeStyle = "#000000";
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.moveTo(yAxisXLeft, 0);
+    ctx.lineTo(yAxisXLeft, height);
+    ctx.stroke();
+
+    for (let i = lMin; i <= hMax; i += (hMax - lMin) / 10) {
+        ctx.font = "10px Arial";
+        ctx.fillText(Math.round(i), yAxisLabelXLeft, convertY(i));
+    }
+}
+
+
+function convertY(y) {
+    return height - y;
 }
